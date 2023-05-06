fe1b77d108009fc66a3a9c92e53f47db3dea2428
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-05-06 16:23:46 -0600

    removed excess comments

--- script.js
+++ script.js
@@ -25,32 +25,4 @@ function addRow() {
   newLastRow.append(row);
 }
 
-// renderCities();
-// //this saves past searches into local storage
-// function renderCities() {
-//   recentCities = [];
-//   previousCities.innerHTML = '';
-//   var pastCities = JSON.parse(localStorage.getItem('pastCities'));
-//   if (pastCities) {
-//     recentCities.push(...pastCities);
-//   }
-//   if (recentCities.length) {
-//     recentCities.forEach((e) => {
-//       var listCity = document.createElement('button');
-//       previousCities.appendChild(listCity);
-//       listCity.textContent = e;
-//       listCity.setAttribute('class', 'past btn btn-secondary w-100 mb-1');
-//       listCity.setAttribute('id', 'previous-cities');
-//     });
-//   }
-//   console.log(pastCities);
-// }
-
 rowButton.addEventListener('click', addRow);
-
-// searchBtn.addEventListener('click', getApi);
-// //TODO: fix this so that the selected item is put in the search bar.
-// previousCities.addEventListener('click', function () {
-//   citySearch.value = previousCities.this.textContent;
-//   getApi();
-// });
