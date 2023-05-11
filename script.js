2169cdd22bbf9c09bb74bf88e69c8d6f3fcfc5f5
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-05-10 18:39:24 -0600

    added start of sum function

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ var lastPurchaseOptions = [];
 
 const rowButton = document.getElementById('new-row');
 
-function addRow() {
+function row() {
+  for (var i = 0; i <= 6; i++) {
+    var x = "<input placeholder='score'/>";
+    var cell = document.createElement('td');
+    newRow.appendChild('cell');
+    cell.innerHTML = x;
+  }
+  return;
+}
+
+function addRow(row) {
   console.log('you clicked the button!!');
   const table = document.getElementById('purchase-table');
   const newRow = document.createElement('tr');
@@ -13,17 +23,13 @@ function addRow() {
   newRow.id = 'last';
   // TODO: make this function to create multiple elements.
   // TODO: make this dynamically add the class name for the columns 1-6.
-  function row() {
-    for (var i = 0; i <= 6; i++) {
-      var x = "<input placeholder='score'/>";
-      var cell = document.createElement('td');
-      newRow.appendChild('cell');
-      cell.innerHTML = x;
-    }
-  }
 
   var newLastRow = document.getElementById('last');
   newLastRow.append(row);
+}
+
+function columnTotals(columns) {
+  let arr = [];
 }
 
 rowButton.addEventListener('click', addRow);
